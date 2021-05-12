1f1053ca24996250f2b928714982163468a1edf6
thuyn789 <thuynh0135@gmail.com>
2021-05-11 21:01:46 -0400

    Added visitor counter

---
+++ script/firebase-key.js
@@ -0,0 +1,22 @@
+// The core Firebase JS SDK is always required and must be listed first -->
+//<script src="https://www.gstatic.com/firebasejs/8.4.3/firebase-app.js"></script>
+//<script src="https://www.gstatic.com/firebasejs/8.4.3/firebase-database.js"></script>
+//<script src="https://www.gstatic.com/firebasejs/8.4.3/firebase-auth.js"></script>
+
+
+//<!-- TODO: Add SDKs for Firebase products that you want to use
+//     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCAN6ltkh5UO6UYwLF3FmJrHt73bCfLgj8",
+    authDomain: "supercool-rental.firebaseapp.com",
+    databaseURL: "https://supercool-rental-default-rtdb.firebaseio.com",
+    projectId: "supercool-rental",
+    storageBucket: "supercool-rental.appspot.com",
+    messagingSenderId: "138927912921",
+    appId: "1:138927912921:web:4a28b2011ffb2605464bc8"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
--- script/main.js
+++ script/main.js
@@ -1,4 +1,9 @@
-function pdfLoader(imgSelector,embSelector) {
+let ipData = getIPData();
+let date = getTodayDate().trim();
+
+console.log(ipData);
+
+function pdfLoader(imgSelector, embSelector) {
 	//load resume img if the current device is a mobile
 	var isMobile = /Android|webOS|iPhone|iPad|iPod|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
@@ -9,3 +14,100 @@ function pdfLoader(imgSelector,embSelector) {
 	}
 }
 
+function getIPData() {
+	let ipLocation = {
+		ipAddress: "",
+		ipCity: "",
+		ipRegion: "",
+		ipCountry:""
+	};
+	$.ajax({
+		async: false,
+		url: "https://ipinfo.io/json",
+		success: function(data) {
+			ipLocation["ipAddress"] = data.ip.trim();
+			ipLocation["ipCity"] = data.city.trim();
+			ipLocation["ipRegion"] = data.region.trim();
+			ipLocation["ipCountry"] = data.country.trim();
+		}
+	});
+	return ipLocation;
+}
+
+function getTodayDate(){
+	let today = new Date();
+	let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+	return date;
+}
+
+function accessCount() {
+
+	let count = 0;
+
+	var countData = {
+		count: count,
+		last_location: ipData["ipAddress"]
+	};
+
+	parentEntry = "access_counting";
+	childEntry = date;
+
+	updateDatabase(parentEntry, childEntry, countData);
+}
+
+function locationCount(){
+
+	let count = 0;
+	//Firebase does not allow entry string contain "."
+	//replace ip with "-"
+	let ipLocation = ipData["ipAddress"];
+
+	var countData = {
+		count: count,
+		date_access: date,
+		ipCity: ipData["ipCity"],
+		ipRegion: ipData["ipRegion"],
+		ipCountry:ipData["ipCountry"]
+	};
+
+	parentEntry = "location_counting";
+
+	//Firebase does not allow entry string contain "."
+	//replace ip with "-"
+	childEntry = ipLocation.replace(/\./g, "-").trim();
+
+	updateDatabase(parentEntry, childEntry, countData);
+}
+
+function updateDatabase(parentEntry,childEntry, dataArray) {
+	const database_ref = firebase.database().ref();
+
+	database_ref.child(parentEntry).child(childEntry).get().then((snapshot) => {
+		if (snapshot.exists()) {
+			let count = snapshot.val().count;
+			count++;
+			dataArray["count"] = count;
+
+			database_ref.child(parentEntry).child(childEntry).update(dataArray, (error) => {
+				if (error) {
+					// The write failed...
+				} else {
+					// Data saved successfully!
+				}
+			});
+		} else {
+			//If the location is not found
+			//Create new entry
+			database_ref.child(parentEntry).child(childEntry).update(dataArray, (error) => {
+				if (error) {
+					// The write failed...
+				} else {
+					// Data saved successfully!
+				}
+			});
+
+		}
+	}).catch((error) => {
+		console.error(error);
+	});
+}
